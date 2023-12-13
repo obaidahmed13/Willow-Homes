fadfac61aac1a783e082c33afe2462cdfee602f0
obaidahmed13 <obaidahmed738@gmail.com>
2023-12-13 16:35:42 -0500

    database connect

--- real-estate/api/index.js
+++ real-estate/api/index.js
@@ -1,7 +1,18 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
 
+// Connecting to database
+mongoose.connect(process.env.MONGO).then(()=> {
+    console.log('Connected to MongoDB');
+}).catch((error) => {
+    console.log(error);
+});
+
+// Using express.js to create a web server 
 const app = express();
 
-app.listen(5173, () => {
-    console.log('Server is running on port 5173')
+app.listen(3000, () => {
+    console.log('Server is running on port 3000')
 })
