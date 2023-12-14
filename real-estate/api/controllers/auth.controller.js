37e526d6ad23770ba8ead39007f5d6fcf1e607ae
obaidahmed13 <obaidahmed738@gmail.com>
2023-12-14 14:35:33 -0500

    signup auth

--- real-estate/api/controllers/auth.controller.js
+++ real-estate/api/controllers/auth.controller.js
@@ -1,9 +1,29 @@
 import User from "../models/user.model.js";
+import bcryptjs from 'bcryptjs';
 
+// Signup asynchronous function taking request (req) and response (res) parameters
 export const signup = async(req, res) => {
+
+    // Extract username, email, and password from the request body
     const{username, email, password} = req.body;
-    const newUser = new User({username, email, password});
-        await newUser.save()
-        res.status(201).json("User created successfully")
+
+    // Hashed password, added salt, using bcrypt
+    const hashedPassword = bcryptjs.hashSync(password,10);
+
+    // Create a new User instance with the hashed password
+    const newUser = new User({username, email, password: hashedPassword});
+    try {
+        // Attempt to save the new user to the database
+        await newUser.save();
+        
+        // If successful, send a 201 status (resource created) and a success message
+        res.status(201).json("User created successfully");
+        
+    } catch (error) {
+
+        // If an error occurs during the database operation, send a 500 status (internal server error) and the error message
+        res.status(500).json(error.message);
+        
+    }
 }
 
