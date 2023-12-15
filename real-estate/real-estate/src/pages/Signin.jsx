d53e29c5ae0eb2a71183380ff5163baa087a18c8
obaidahmed13 <obaidahmed738@gmail.com>
2023-12-14 19:54:28 -0500

    add redux toolkit

--- real-estate/real-estate/src/pages/Signin.jsx
+++ real-estate/real-estate/src/pages/Signin.jsx
@@ -1,11 +1,13 @@
 import { useState } from 'react';
 import {Link, useNavigate} from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import {signInStart, signInSuccess,  signInFailure} from '../redux/user/userSlice'
 
 export const Signin = () => {
   const [formData, setFormData] = useState({})
-  const[error, setError] = useState(null)
-  const[loading, setLoading] = useState(false)
+  const {loading, error} = useSelector((state)=> state.user);
   const navigate = useNavigate();
+  const dispath = useDispatch();
   const handleChange = (e) => {
     setFormData({
       ...formData,
@@ -16,29 +18,25 @@ export const Signin = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      setLoading(true);
-
-    const res = await fetch ('/api/auth/signin', {
-      method: 'POST',
-      headers: {
-        'Content-Type' : 'application/json',
-      },
-      body: JSON.stringify(formData),
-    });
-    const data = await res.json();
-    if (data.success === false) {
-      setError(data.message);
-      setLoading(false);
-      return;
-    }
-    setLoading(false);
-    setError(null)
-    navigate('/');
-    } catch (error) {
-      setError(error.message)
-      setLoading(false)
-    }
-  };
+      dispath(signInStart());
+      const res = await fetch ('/api/auth/signin', {
+        method: 'POST',
+        headers: {
+          'Content-Type' : 'application/json',
+        },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      if (data.success === false) {
+        dispath(signInFailure(data.message));
+        return;
+      }
+      dispath(signInSuccess(data));
+      navigate('/');
+      } catch (error) {
+        dispath(signInFailure(error.message));
+      }
+    };
 
   return (
     <div className="p-3 max-w-lg mx-auto">
