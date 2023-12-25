b523d5d25b0e315ec82cfcef3ac9503554204165
Obaid Ahmed <138173339+obaidahmed13@users.noreply.github.com>
2023-12-24 22:42:54 -0500

    city, country in update listing

--- real-estate/real-estate/src/pages/UpdateListing.jsx
+++ real-estate/real-estate/src/pages/UpdateListing.jsx
@@ -13,6 +13,8 @@ export default function UpdateListing() {
         name: '',
         description: '',
         address: '',
+        city: '',
+        country: '',
         type: 'rent',
         bedrooms: 1,
         bathrooms: 1,
@@ -178,6 +180,24 @@ export default function UpdateListing() {
                   required
                   onChange={handleChange} 
                   value={formData.address}
+                />
+                 <input 
+                  type="text" 
+                  placeholder="City" 
+                  className="border p-3 rounded-lg" 
+                  id="city" 
+                  required
+                  onChange={handleChange} 
+                  value={formData.city}
+                />
+                <input 
+                  type="text" 
+                  placeholder="Country" 
+                  className="border p-3 rounded-lg" 
+                  id="country" 
+                  required
+                  onChange={handleChange} 
+                  value={formData.country}
                 />
                 <div className="flex gap-6 flex-wrap">
                 <div className="flex gap-2">
